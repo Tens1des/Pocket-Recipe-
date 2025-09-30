8df3bc1a1d80f0ea7b84aa967d5113c0d081ffab
Tens1des <romankotov3213@gmail.com>
2025-09-30 14:40:18 +0300

    Initial Commit

---
+++ Pocket Recipe Box/Pocket_Recipe_BoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pocket_Recipe_BoxApp.swift
+//  Pocket Recipe Box
+//
+//  Created by Рома Котов on 30.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct Pocket_Recipe_BoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
